3d21be3aea7242e62ec9106e2576279425992dd0
jyheo98 <67140978+jyheo98@users.noreply.github.com>
2022-07-18 07:18:00 +0900

    [Bronze IV] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+	#include "headers/debug.h"
+#else
+  	#define debug(...) 42
+#endif
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+#define LLINF 0x3f3f3f3f3f3f3f3f
+#define INF 0x3f3f3f3f
+#define uniq(x) sort(all(x)); x.resize(unique(all(x))-x.begin());
+#define sz(x) (int)x.size()
+#define pw(x) (1LL<<x)
+
+using pii = pair<int, int>;
+using ll = long long;
+using ld = long double;
+const ll MOD = 1e9 + 7;
+const long double PI = acos(-1.0);
+
+int main() {
+	ios::sync_with_stdio(0), cin.tie(0);
+	int t; cin >> t;
+	while(t--) {
+		int a, b; cin >> a >> b;
+		cout << a + b << '\n';
+	}
+}
