e5f8fe91a91b3a78e9052981411b1782fbbe18c9
jyheo98 <67140978+jyheo98@users.noreply.github.com>
2022-07-18 07:14:00 +0900

    [Bronze V] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+	#include "headers/debug.h"
+#else
+  	#define debug(...) 42
+#endif
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+#define LLINF 0x3f3f3f3f3f3f3f3f
+#define INF 0x3f3f3f3f
+#define uniq(x) sort(all(x)); x.resize(unique(all(x))-x.begin());
+#define sz(x) (int)x.size()
+#define pw(x) (1LL<<x)
+
+using pii = pair<int, int>;
+using ll = long long;
+using ld = long double;
+const ll MOD = 1e9 + 7;
+const long double PI = acos(-1.0);
+
+int main() {
+	ios::sync_with_stdio(0), cin.tie(0);
+	ll tot; cin >> tot;
+	int n; cin >> n;
+	for(int i=0 ; i<n ; i++) {
+		ll price, quantity; cin >> price >> quantity;
+		tot -= price * quantity;
+	}
+	if(tot == 0) cout << "Yes\n";
+	else cout << "No\n";
+}
