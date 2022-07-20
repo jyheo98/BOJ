e4667b4ecce12c92573b8486976e333765fbb058
jyheo98 <67140978+jyheo98@users.noreply.github.com>
2022-07-21 02:06:47 +0900

    [Gold I] Title: 구간 합 구하기, Time: 352 ms, Memory: 80224 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,161 +1,130 @@
-#define LOCAL
-
-#undef _GLIBCXX_DEBUG
-
 #include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+	#include "headers/debug.h"
+#else
+  	#define debug(...) 42
+#endif
+
 using namespace std;
 
-#define IOS ios::sync_with_stdio(false);cin.tie(0)
 #define all(x) x.begin(), x.end()
 #define ff first
 #define ss second
-#define LLINF 1e18
-#define INF (int)1e9+1
+#define LLINF 0x3f3f3f3f3f3f3f3f
+#define INF 0x3f3f3f3f
+#define uniq(x) sort(all(x)); x.resize(unique(all(x))-x.begin());
+#define sz(x) (int)x.size()
+#define pw(x) (1LL<<x)
 
-// Copied from Gennady-Korotkevich's template
+using pii = pair<int, int>;
+using ll = long long;
+using ld = long double;
+const ll MOD = 1e9 + 7;
+const long double PI = acos(-1.0);
 
-template <typename A, typename B>
-string to_string(pair<A, B> p);
 
-template <typename A, typename B, typename C>
-string to_string(tuple<A, B, C> p);
+struct node {
+    node* l;
+    node* r;
+    node* p;
+    ll sum;
+    ll v;
+    ll cnt;
+    node(ll k,node* pp) {
+        p = pp;
+        l = r = nullptr;
+        v = k;
+        cnt = 1;
+        sum = k;
+    }
+} *root;
 
-template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p);
-
-string to_string(const string& s) {
-	return '"' + s + '"';
+void update(node *x){
+    x->cnt = 1;
+    x->sum = x->v;
+    if(x->l){
+        x->cnt += x->l->cnt;
+    	x->sum += x->l->sum;
+    }
+    if(x->r){
+        x->cnt += x->r->cnt;
+        x->sum += x->r->sum;
+    }
+  
+}
+void rotate(node* x){//x to be parent
+    node* p=x->p;
+    node* b=nullptr;
+    if(!p)return;
+    if(x==p->l) p->l=b=x->r,x->r=p;
+    else p->r=b=x->l,x->l=p;
+    x->p=p->p;p->p=x;
+    if(b) b->p=p;
+    if(x->p){
+        if(x->p->l==p)
+            x->p->l=x;
+        else x->p->r=x;
+    }else root=x;
+    update(p);update(x);
+}
+void splay(node *x,node *g=nullptr){//x to be g's child
+    while(x->p!=g){
+        node* p=x->p;
+        if(p->p==g){rotate(x);break;}
+        node* pp = p->p;
+        if((p->l==x)^(pp->l==p)) {rotate(x);rotate(x);}
+        else {rotate(p);rotate(x);}
+    }
+    if(!g)root=x;
+}
+void kth(ll k){
+    node* x=root;
+    while(1){
+        while(x->l&&x->l->cnt>k)x=x->l;
+        if(x->l)k-=x->l->cnt;
+        if(!k)break;k--;
+        x=x->r;
+    }
+    splay(x);
+}
+void gather(int s,int e){
+    kth(e+1);
+    node* temp=root;
+    kth(s-1);
+    splay(temp,root);
+    return;
 }
 
-string to_string(const char* s) {
-	return to_string((string)s);
+node* ptr[1000100];
+ll arr[1001000];
+void init(ll n){
+    if(root) delete root;
+    node* now=new node(-INF, nullptr);
+    root=now;
+    for(ll i=1; i<=n; i++){
+        ptr[i] =  now->r = new node(arr[i],now);
+        now = now->r;
+    }
+    now->r=new node(INF, now);
+    for(ll i=n; i>=1; i--) update(ptr[i]);
+    splay(ptr[n/2+1]);
 }
-
-string to_string(bool b) {
-	return (b ? "true" : "false");
-}
-
-string to_string(vector<bool> v) {
-	bool first = true;
-	string res = "{";
-	for (int i = 0; i < static_cast<int>(v.size()); i++) {
-		if (!first) {
-			res += ", ";
-		}
-		first = false;
-		res += to_string(v[i]);
-	}
-	res += "}\n";
-	return res;
-}
-
-template <size_t N>
-string to_string(bitset<N> v) {
-	string res = "";
-	for (size_t i = 0; i < N; i++) {
-		res += static_cast<char>('0' + v[i]);
-	}
-	return res;
-}
-
-template <typename A>
-string to_string(A v) {
-	bool first = true;
-	string res = "{";
-	for (const auto& x : v) {
-		if (!first) {
-			res += ", ";
-		}
-		first = false;
-		res += to_string(x);
-	}
-	res += "}\n";
-	return res;
-}
-
-template <typename A, typename B>
-string to_string(pair<A, B> p) {
-	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
-}
-
-template <typename A, typename B, typename C>
-string to_string(tuple<A, B, C> p) {
-	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
-}
-
-template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p) {
-	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
-}
-
-template <typename A, typename B, typename C, typename D, typename E>
-string to_string(tuple<A, B, C, D, E> p) {
-	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + "," + to_string(get<4>(p)) + ")";
-}
-
-void debug_out() {
-	cerr << endl;
-}
-
-template <typename Head, typename... Tail>
-void debug_out(Head H, Tail... T) {
-	cerr << " " << to_string(H);
-	debug_out(T...);
-}
-
-#ifdef LOCAL
-#define debug(...) cerr << "\n[" << #__VA_ARGS__ << "]:\n", debug_out(__VA_ARGS__)
-#else
-#define debug(...) 42
-#endif
-
-// End of Gennady-Korotkevich's template 
-
-using llong = long long;
-using PII = pair<int, int>;
-
-const llong MOD = 1e9 + 7;
-const double PI = acos(-1);
-
-const int N = 1000100;
-llong seg[N * 4], a[N];
-int n, m, k;
-
-llong build(int idx, int ll, int rr) {
-	if (ll == rr) return seg[idx] = a[ll];
-	int mid = (ll + rr) >> 1;
-	return seg[idx] = build(idx * 2, ll, mid) + build(idx * 2 + 1, mid + 1, rr);
-}
-
-llong upd(int idx, int ll, int rr, int ch, llong val) {
-	if (ll == rr && ll == ch) return seg[idx] = val;
-	if (ch < ll || ch > rr) return seg[idx];
-	int mid = (ll + rr) >> 1;
-	return seg[idx] = upd(idx * 2, ll, mid, ch, val) + upd(idx * 2 + 1, mid + 1, rr, ch, val);
-}
-
-llong sum(int idx, int ll, int rr, int ql, int qr) {
-	if (ql > qr) return 0;
-	if (ll == ql && rr == qr) return seg[idx];
-	int mid = (ll + rr) >> 1;
-	return sum(idx * 2, ll, mid, ql, min(qr, mid)) 
-		+ sum(idx * 2 + 1, mid + 1, rr, max(mid + 1, ql), qr);
-}
-
 int main() {
-	IOS;
-	cin >> n >> m >> k;
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	build(1, 0, n - 1);
-	for (int i = 0; i < m + k; i++) {
-		llong q, l, r; cin >> q >> l >> r;
-		if (q == 1) {
-			upd(1, 0, n-1, l-1, r);
-		}
-		if (q == 2) {
-			cout << sum(1, 0, n-1, l-1, r-1) << "\n";
+	ios::sync_with_stdio(0), cin.tie(0);
+	int n, p, q; cin >> n >> p >> q; p+=q;
+	for(int i=1 ; i<=n ; i++) cin >> arr[i];
+	init(n);
+	while(p--) {
+		int op; cin >> op;
+		if(op == 1) {
+			int id; ll val; cin >> id >> val;
+			kth(id);
+			root->v=val;
+			update(root);
+		} else {
+			int l, r; cin >> l >> r;
+			gather(l,r);
+			cout<<root->r->l->sum<<'\n';
 		}
 	}
 }
