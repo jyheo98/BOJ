6e6e5069329761a6af71436e59808bafe3640bc6
jyheo98 <67140978+jyheo98@users.noreply.github.com>
2022-07-28 15:25:59 +0900

    [Platinum V] Title: 찾기, Time: 56 ms, Memory: 9428 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -0,0 +1,170 @@
+#define LOCAL
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2")
+
+#define IOS ios::sync_with_stdio(false);cin.tie(0)
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+#define LLINF 0x3f3f3f3f3f3f3f3f
+#define INF 0x3f3f3f3f
+#define uniq(x) sort(all(x)); x.resize(unique(all(x))-x.begin());
+#define sz(x) (int)x.size()
+
+// Copied from Gennady-Korotkevich's template
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+	return to_string((string)s);
+}
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast<int>(v.size()); i++) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(v[i]);
+	}
+	res += "}\n";
+	return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast<char>('0' + v[i]);
+	}
+	return res;
+}
+
+template <typename A>
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto& x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}\n";
+	return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D, typename E>
+string to_string(tuple<A, B, C, D, E> p) {
+	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + "," + to_string(get<4>(p)) + ")";
+}
+
+void debug_out() {
+	cerr << endl;
+}
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "\n[" << #__VA_ARGS__ << "]:\n", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+// End of Gennady-Korotkevich's template 
+
+using pii = pair<int, int>;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const long double PI = acos(-1.0);
+
+string p, s;
+vector<int> fail, ans;
+
+void find_fail() {
+	int n = sz(p), j = 0;
+	for(int i=1 ; i<n ; i++) {
+		while(j>0 && p[i]!=p[j])
+			j = fail[j-1];
+		if(p[i]==p[j])
+			fail[i] = ++j;
+	}
+}
+
+void kmp() {
+	int n = sz(s), m = sz(p), j=0;
+	for(int i=0 ; i<n ; i++) {
+		while(j>0 && s[i]!=p[j])
+			j = fail[j-1];
+		if(s[i]==p[j]) {
+			if(j==m-1) {
+				ans.push_back(i-m+1);
+				j = fail[j];
+			} else
+				j++;
+		}
+	}
+}
+
+void solve() { 
+	getline(cin, s);
+	getline(cin, p);
+	fail = vector<int>(sz(p));
+	find_fail();
+	kmp();
+	cout << sz(ans) << "\n";
+	for(auto x : ans) {
+		cout << x+1 << " ";
+	}
+	cout << "\n";
+}
+
+int main() {
+	IOS;
+	int t; t = 1;
+	while(t--)
+		solve();
+}
